a7ba982d4ad48c53c0f56a4c3afbccb2d5297012
Jamshid Sourati <jsourati@midway2-login1.rcc.local>
2019-11-19 10:59:32 -0600

    Data utility module that provides a class for preprocessing and storing abstracts as training data

---
+++ data/utils.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import pdb
+
+import numpy as np
+import pandas as pd
+
+mat2vec_path = '~/scratch-midway2/repos/mat2vec'
+
+from pybliometrics.scopus import AbstractRetrieval
+from mat2vec.processing.process import MaterialsTextProcessor
+
+class MatTextProcessor(MaterialsTextProcessor):
+
+    def mat_preprocess(self, text):
+        """Pre-processing a given text using tools provided by 
+        the repository mat2vec 
+
+
+        """
+
+        tokens = self.tokenize(text)
+        ptokens = [self.process(token)[0] for token in tokens]
+        pptokens = [self.make_phrases(token) for token in ptokens]
+
+        return pptokens   
+
+    def make_training_file(self, dois, save_dir):
+        """Downloading, pre-processsing and storing abstracts of a set
+        of DOIs in a text file which can be later used as the training data
+        for tuning models like word2vec
+
+        Each line of the saved file corresponds to one article and shows
+        its title followed by the abstract
+
+        ** Parameters:
+            * dois : *(list)* list of DOIs
+            * saved_dir : *(str)* directory to save the files
+        """
+
+        # list of lists (each list = one line = title + abstract)
+        save_path_abst = os.path.join(save_dir, 'abstracts')
+        save_path_dois = os.path.join(save_dir, 'saved_DOIs')
+        saved_dois = []
+        for doi in dois:
+            try:
+                r = AbstractRetrieval(doi)
+            except:
+                continue
+
+            tokens = self.mat_preprocess(r.title) + [['.']] + self.mat_preprocess(r.description)
+            line = ' '.join(sum(tokens[:-3],[]))
+            doi_line = doi
+            if doi!=dois[-1]:
+                line += '\n'
+                doi_line += '\n'
+
+            # saving the texts
+            with open(save_path_abst, 'a+', encoding='utf-8') as f:
+                f.write(line)
+            with open(save_path_dois, 'a+') as f:
+                f.write(doi_line)
