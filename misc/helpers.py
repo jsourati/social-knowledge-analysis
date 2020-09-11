244c528ce2d333b0b0073f45803567dcbc197ef4
Jamshid Sourati <jsourati@uchicago.edu>
2020-09-11 13:33:08 -0500

    Removing dots from deepwalk sentences

--- misc/helpers.py
+++ misc/helpers.py
@@ -65,9 +65,34 @@ def prune_deepwalk_sentences(sents, remove='author'):
     hl = [' '.join(h) for h in hl if len(h)>1]
 
     # removing dots
-    #hl = [h.split('.')[0] for h in hl]
+    hl = [h.split('.')[0] for h in hl]
 
     # removing those sentences only containing the keyword
     hl = [h for h in hl if len(np.unique(h.split(' ')))>1]
 
     return hl
+
+
+def lighten_color(color, amount=0.5):
+    """
+    Downloaded
+    -----------
+    This piece of code is downloaded from
+    https://stackoverflow.com/a/49601444/8802212
+
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
