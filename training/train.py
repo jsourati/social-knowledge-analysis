eb35a2576a0a77fa77e70f99722987122ae4e16d
Jamshid Sourati <jsourati@uchicago.edu>
2020-09-11 13:32:21 -0500

    Adding a new class for matrix factorization based word2vec

--- training/train.py
+++ training/train.py
@@ -4,7 +4,10 @@ import pdb
 import json
 import logging
 import numpy as np
+from scipy import sparse
+from collections import Counter
 
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LogisticRegressionCV
 
@@ -157,3 +160,137 @@ class MyCallBack(CallbackAny2Vec):
                                                                              model.min_alpha_yet_reached))
 
 
+                
+class PPMI(object):
+
+    def __init__(self, path_to_sents, **kwargs):
+
+        self.sents = open(path_to_sents,'r').read().splitlines()
+        
+        # for now we are skipping the preprocessing steps as we
+        # will be using this for deepwalk sentences that do not need
+        # preprocessing
+        # ... PREPROCESSING GOES HERE
+        self.to_be_removed = []
+
+        # setting up the logger
+        logger_disable = kwargs.get('silent', False)
+        self.logger = helpers.set_up_logger(__name__, None, logger_disable)
+
+        
+    def form_unigrams(self):
+
+        self.uni_counts = Counter()
+        for iD, D in enumerate(self.sents):
+            tokens = [d for d in D.split(' ') if d not in self.to_be_removed]
+            for i, tok in enumerate(tokens):
+                # unigram count
+                self.uni_counts[tok] += 1
+
+            if not(iD%10000):
+                self.logger.info(iD)
+
+        self.tok2ind = {tok: ind for ind, tok in enumerate(self.uni_counts.keys())}
+        self.ind2tok = {ind: tok for tok,ind in self.tok2ind.items()}
+                
+                
+    def form_skipgrams(self, w):
+
+        self.w = w
+        win = int((w-1)/2)
+
+        self.skip_counts = Counter()
+        for iD, D in enumerate(self.sents):
+            tokens = [d for d in D.split(' ') if d in self.uni_counts]
+
+            for i, tok in enumerate(tokens):
+                # pairwise count
+                start_window = max(i-win, 0)
+                end_window   = min(i+win, len(tokens)-1)
+                context = [tokens[ii] for ii in np.arange(start_window, end_window+1) if ii!=i]
+                for c in context:
+                    self.skip_counts[tok, c] += 1
+
+            if not(iD%10000):
+                self.logger.info(iD)
+
+
+    def form_sparse_cnt_matrix(self, save_path=None):
+
+        nV = len(self.uni_counts)
+        self.cmat = sparse.lil_matrix((nV,nV), dtype=np.uint32)
+        rows=[]
+        cols=[]
+        vals=[]
+        for i,skp in enumerate(self.skip_counts.items()):
+            # each element of the skipgram counter is like
+            # [..., (('center','cntxt'), count), ...]
+            # index of the center (=row)
+            rows += [self.tok2ind[skp[0][0]]]
+            # index of the context (=column)
+            cols += [self.tok2ind[skp[0][1]]]
+            # pairwise counts of center and context tokens
+            vals += [skp[1]]
+
+            if not(i%1000000) or (i==len(self.skip_counts)-1):
+                self.logger.info(i)
+                self.cmat[rows,cols] = vals
+                rows=[]
+                cols=[]
+                vals=[]
+
+        self.cmat = self.cmat.tocsr()
+        
+        if save_path is not None:
+            sparse.save_npz(self.cmat, save_path)
+
+
+    def form_sppmi_matrix(self):
+                                                                                
+        nV = len(self.uni_counts)
+        self.sppmi = sparse.lil_matrix((nV,nV), dtype=np.float32)
+
+        word_cnts = np.array(self.cmat.sum(1)).flatten()
+        ncorp = word_cnts.sum()
+        shift = np.log(self.w)
+
+        rows = []
+        cols = []
+        vals = []
+        for ind,tok in self.ind2tok.items():
+            nnz_cols = self.cmat[ind,:].indices
+            wj_cnts = word_cnts[nnz_cols]
+            wi_cnt  = word_cnts[ind]
+
+            # associatedness
+            assocs = ncorp*self.cmat[ind,:].data / (wi_cnt*wj_cnts)
+            # shifted ppmi
+            sppmi_vals = np.log(assocs) - shift
+            p_cols = nnz_cols[sppmi_vals>0]
+
+            rows += list(np.ones(len(p_cols), dtype=np.int32)*ind)
+            cols += list(p_cols)
+            vals += list(sppmi_vals[sppmi_vals>0])
+
+            if not(ind%10000) or (ind==nV-1):
+                self.logger.info(ind)
+                self.sppmi[rows,cols] = vals
+                rows = []
+                cols = []
+                vals = []
+
+
+    def get_most_similar_terms(self, tok, words=None, ntop=10, thrld=0, use_SVD=False):
+
+        if words is None:
+            words_inds = np.array([y for x,y in self.tok2ind.items()
+                                   if self.uni_counts[x]>thrld])
+        else:
+            words_inds = np.array([self.tok2ind[x] for x in words
+                                   if self.uni_counts[x]>thrld])
+
+        cosine_sims = cosine_similarity(self.cmat[[self.tok2ind[tok]],:],
+                                        self.cmat[words_inds,:]).flatten()
+        most_similar_inds = words_inds[np.argsort(-cosine_sims)[:ntop]]
+
+        return [(self.ind2tok[x], cosine_sims[x]) for x in most_similar_inds]
