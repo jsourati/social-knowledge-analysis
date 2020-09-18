d678c04df9f7db2686b62f02458e88486fefd319
Jamshid Sourati <jsourati@uchicago.edu>
2020-09-18 15:18:22 -0500

    Adding detector of studied entities; and adding a ground-truth (co-occurrence-based) discovery detector using sparse vertex weight matrices

--- misc/helpers.py
+++ misc/helpers.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import copy
+import json
 import logging
 import numpy as np
 
@@ -39,15 +40,86 @@ def locate_array_in_array(moving_arr, fixed_arr):
     locs_in_sorted = np.searchsorted(fixed_arr[sinds], moving_arr)
 
     return sinds[locs_in_sorted]
-    
 
-def find_first_time_cocrs(cocrs, col):
-    """Detecting first-time cooccurrences in a given column
-    of the cooccurrence matrix
+
+def find_studied_ents_VW(ents,VW,row_yrs,yr):
+    """Generating entities that have been studied prior to the input 
+    year based on a given vertex-weight matrix 
+
+    Here, it is assumed that VW (vertex weight matrix) contains only
+    the entity+property columns (i.e., author columns are excluded).
     """
 
-    bin_indic = (np.sum(cocrs[:,:col],axis=1)==0) * (cocrs[:,col]>0)
-    return np.where(bin_indic)[0]
+    assert len(ents)==(VW.shape[1]-1), 'Number of columns in the vertex weight ' +\
+        'matrix should equal the number of given entities.'
+
+    assert len(row_yrs)==VW.shape[0], 'Number of rows in the vertex weight ' +\
+        'matrix should equal the number of given years.'
+
+    sub_VW = VW[row_yrs<yr,:]
+    studied_bin = np.asarray(np.sum(sub_VW[:,:-1].multiply(sub_VW[:,-1]), axis=0)>0)[0,:]
+    return ents[studied_bin]
+    
+
+def find_studied_ents_linksdict(file_or_dict,yr):
+    """Generating entities that have been studied prior to the input 
+    year (excluding that year) based on a dictionary of the form:
+    {E1:Y1, E2:Y2, ...}
+    where Ei's are the entities and Yi's are the corresponding years that the 
+    relationship between Ei's and the property are obtained (curated).
+    """
+
+    if isinstance(file_or_dict,str):
+        link_dict = json.load(open(file_or_dict,'r'))
+    elif isinstance(file_or_dict, dict):
+        link_dict = file_or_dict
+    else:
+        raise ValueError('The first input should be either a string (path ' +\
+                         'to file) or a dictionary.')
+
+    return np.array([x for x,y in link_dict.items() if y<yr])
+
+
+def gt_discoveries(ents,VW,row_yrs,constraint_func=None):
+    """Generating ground truth discoveries in a given year
+    """
+
+    assert len(ents)==(VW.shape[1]-1), 'Number of columns in the vertex weight ' +\
+                'matrix should equal the number of given entities.'
+
+    assert len(row_yrs)==VW.shape[0], 'Number of rows in the vertex weight ' +\
+        'matrix should equal the number of given years.'
+
+    def gt_disc_func(year_of_gt):
+
+        sub_VW = VW[row_yrs==year_of_gt,:]
+        studied_bin = np.asarray(np.sum(sub_VW[:,:-1].multiply(sub_VW[:,-1]), axis=0)>0)[0,:]
+        all_studied_ents = ents[studied_bin]
+        # remove already studied ones
+        prev_studied_ents = find_studied_ents_VW(ents,VW,row_yrs,year_of_gt)
+
+        disc_ents = all_studied_ents[~np.isin(all_studied_ents,prev_studied_ents)]
+
+        if constraint_func is not None:
+            disc_ents = constraint_func(disc_ents)
+        
+        return disc_ents
+
+    return gt_disc_func
+
+
+def gt_discoveries_4CTD(disease):
+
+    ds_dr_path = '/home/jamshid/codes/data/CTD/diseases_drugs.json'
+    target_ds_dr = json.load(open(ds_dr_path, 'r'))
+    rel_drugs = target_ds_dr[disease]
+
+    def gt_disc_func(yr):
+        gt = np.array([x.lower() for x,y in rel_drugs.items() if int(y)==yr])
+        gt = np.array([x.replace(' ','_') for x in gt])
+        return gt
+
+    return gt_disc_func
 
 
 def prune_deepwalk_sentences(sents, remove='author'):
